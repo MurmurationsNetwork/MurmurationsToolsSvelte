f3aa0f4f1173f5927357e5c1aec3b4406f9e0d3c
Geoff Turk <geoff@ic3.dev>
2024-07-12 09:59:25 +0200

    feat: setup profile generator layout

--- src/routes/profile-generator/+page.svelte
+++ src/routes/profile-generator/+page.svelte
@@ -1,1 +1,12 @@
-<h1>Profile Generator</h1>
+<div class="container mx-auto flex justify-center items-top">
+	<div class="text-center md:flex flex-row grow items-top">
+		<!-- BEGIN: List of user-generated profiles -->
+		<div class="bg-blue-50 md:basis-1/3 m-2">Profiles List</div>
+		<!-- END: List of user-generated profiles -->
+		<!-- BEGIN: Schema selection box / Create/modify profile input / Profile preview -->
+		<div class="md:basis-2/3 md:order-first p-2">
+			Schemas Selection / Profile Input / Profile Preview
+		</div>
+		<!-- END: Schema selection box / Create/modify profile input / Profile preview -->
+	</div>
+</div>
