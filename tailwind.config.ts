c7a6bd06f5f403aaa1ea0092b9b2c65053d5d9d8
Geoff Turk <geoff@ic3.dev>
2024-07-15 15:29:40 +0200

    feat: initial setup with murm theme

---
+++ tailwind.config.ts
@@ -0,0 +1,26 @@
+import { join } from 'path';
+import type { Config } from 'tailwindcss';
+import forms from '@tailwindcss/forms';
+import typography from '@tailwindcss/typography';
+import { skeleton } from '@skeletonlabs/tw-plugin';
+import { MurmurationsTheme } from './murmurations-theme';
+
+export default {
+	darkMode: 'media',
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')
+	],
+	theme: {
+		extend: {}
+	},
+	plugins: [
+		forms,
+		typography,
+		skeleton({
+			themes: {
+				custom: [MurmurationsTheme]
+			}
+		})
+	]
+} satisfies Config;
