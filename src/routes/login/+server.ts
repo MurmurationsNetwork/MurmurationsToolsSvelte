89a4bb8d8b370c29d39e9fe7280597b2e8cbb4e6
vthwang <vincent@fishboneapps.com>
2024-09-18 11:16:22 -0700

    refactor: refactor login part
    Signed-off-by: vthwang <vincent@fishboneapps.com>

--- src/routes/login/+server.ts
+++ src/routes/login/+server.ts
@@ -7,119 +7,97 @@ import { v4 as uuidv4 } from 'uuid';
 import { serialize } from 'cookie';
 import { PUBLIC_ENV } from '$env/static/public';
 import { GenerateCuid } from '$lib/utils';
+import type { Db } from 'mongodb';
 
 export const POST: RequestHandler = async ({ request }) => {
 	try {
 		const { email, password, loginType } = await request.json();
 
 		if (!email || !password || !loginType) {
-			return json({ success: false, error: 'Missing required fields' }, { status: 400 });
+			return jsonError('Missing required fields', 400);
 		}
 
-		// Hash the email using SHA256
 		const emailHash = crypto.createHash('sha256').update(email).digest('hex');
-
 		const db = await connectToDatabase();
 
-		if (loginType === 'register') {
-			// Check if the user already exists
-			const existingUser = await db.collection('users').findOne({ email_hash: emailHash });
-			if (existingUser) {
-				return json({ success: false, error: 'User already exists' }, { status: 400 });
-			}
-
-			// Hash the password using bcrypt
-			const hashedPassword = await bcrypt.hash(password, 10);
-
-			// Generate a CUID for the user
-			const cuid = GenerateCuid();
-
-			// Create the new user object
-			const newUser = {
-				cuid,
-				email_hash: emailHash,
-				password: hashedPassword,
-				ipfs: '',
-				ipns: '',
-				last_login: Date.now(),
-				profiles: []
-			};
-
-			await db.collection('users').insertOne(newUser);
-
-			// Generate session
-			const sessionToken = uuidv4();
-			await db.collection('sessions').insertOne({
-				session_token: sessionToken,
-				email_hash: emailHash,
-				created_at: new Date()
-			});
-
-			await closeDatabaseConnection();
-
-			// Set the session cookie
-			return new Response(
-				JSON.stringify({ success: true, message: 'Registration successful', userEmail: email }),
-				{
-					status: 200,
-					headers: {
-						'Set-Cookie': serialize('murmurations_tools_session', sessionToken, {
-							httpOnly: true,
-							path: '/',
-							secure: PUBLIC_ENV === 'production',
-							maxAge: 60 * 60 * 24 * 7
-						}),
-						'Content-Type': 'application/json'
-					}
-				}
-			);
-		} else if (loginType === 'login') {
-			const user = await db.collection('users').findOne({ email_hash: emailHash });
-
-			if (!user || !user.password) {
-				return json({ success: false, error: 'Invalid credentials' }, { status: 401 });
-			}
-
-			const isCorrectPassword = await bcrypt.compare(password, user.password);
-
-			if (!isCorrectPassword) {
-				return json({ success: false, error: 'Invalid credentials' }, { status: 401 });
-			}
-
-			// Generate session
-			const sessionToken = uuidv4();
-			await db.collection('sessions').insertOne({
-				session_token: sessionToken,
-				email_hash: user.email_hash,
-				created_at: new Date()
-			});
-
-			await db
-				.collection('users')
-				.updateOne({ email_hash: emailHash }, { $set: { last_login: Date.now() } });
-
-			await closeDatabaseConnection();
-
-			return json(
-				{ success: true, message: 'Login successful', userEmail: email },
-				{
-					status: 200,
-					headers: {
-						'Set-Cookie': serialize('murmurations_tools_session', sessionToken, {
-							httpOnly: true,
-							path: '/',
-							secure: PUBLIC_ENV === 'production',
-							maxAge: 60 * 60 * 24 * 7
-						}),
-						'Content-Type': 'application/json'
-					}
-				}
-			);
-		} else {
-			return json({ success: false, error: 'Invalid action' }, { status: 400 });
+		switch (loginType) {
+			case 'register':
+				return await handleRegistration(db, emailHash, password, email);
+			case 'login':
+				return await handleLogin(db, emailHash, password, email);
+			default:
+				return jsonError('Invalid action', 400);
 		}
 	} catch (err) {
 		console.error(`Login/Register failed: ${err}`);
-		return json({ success: false, error: 'Internal server error' }, { status: 500 });
+		return jsonError('Internal server error', 500);
 	}
 };
+
+const handleRegistration = async (db: Db, emailHash: string, password: string, email: string) => {
+	const existingUser = await db.collection('users').findOne({ email_hash: emailHash });
+	if (existingUser) {
+		return jsonError('User already exists', 400);
+	}
+
+	const hashedPassword = await bcrypt.hash(password, 10);
+	const cuid = GenerateCuid();
+
+	const newUser = {
+		cuid,
+		email_hash: emailHash,
+		password: hashedPassword,
+		ipfs: '',
+		ipns: '',
+		last_login: Date.now(),
+		profiles: []
+	};
+	await db.collection('users').insertOne(newUser);
+
+	const sessionToken = await createSession(db, emailHash);
+	await closeDatabaseConnection();
+
+	return createSuccessResponse(email, sessionToken, 'Registration successful');
+};
+
+const handleLogin = async (db: Db, emailHash: string, password: string, email: string) => {
+	const user = await db.collection('users').findOne({ email_hash: emailHash });
+	if (!user || !(await bcrypt.compare(password, user.password))) {
+		return jsonError('Invalid credentials', 401);
+	}
+
+	await db
+		.collection('users')
+		.updateOne({ email_hash: emailHash }, { $set: { last_login: Date.now() } });
+	const sessionToken = await createSession(db, emailHash);
+	await closeDatabaseConnection();
+
+	return createSuccessResponse(email, sessionToken, 'Login successful');
+};
+
+const createSession = async (db: Db, emailHash: string) => {
+	const sessionToken = uuidv4();
+	await db.collection('sessions').insertOne({
+		session_token: sessionToken,
+		email_hash: emailHash,
+		created_at: new Date()
+	});
+	return sessionToken;
+};
+
+const createSuccessResponse = (email: string, sessionToken: string, message: string) => {
+	return new Response(JSON.stringify({ success: true, message, userEmail: email }), {
+		status: 200,
+		headers: {
+			'Set-Cookie': serialize('murmurations_tools_session', sessionToken, {
+				httpOnly: true,
+				path: '/',
+				secure: PUBLIC_ENV === 'production',
+				maxAge: 60 * 60 * 24 * 7
+			}),
+			'Content-Type': 'application/json'
+		}
+	});
+};
+
+const jsonError = (error: string, status: number) => json({ success: false, error }, { status });
