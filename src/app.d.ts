9320ca027e7942a90eb50e65d9c267b0daf74b89
vthwang <vincent@fishboneapps.com>
2025-01-06 16:46:07 -0800

    fix: fix any type error
    Signed-off-by: vthwang <vincent@fishboneapps.com>

--- src/app.d.ts
+++ src/app.d.ts
@@ -20,7 +20,7 @@ declare global {
 				DB: D1Database;
 			};
 			context: {
-				waitUntil(promise: Promise<any>): void;
+				waitUntil(promise: Promise<unknown>): void;
 			};
 			caches: CacheStorage & { default: Cache };
 		}
--- src/lib/db/db.ts
+++ src/lib/db/db.ts
@@ -1,7 +1,7 @@
-import { drizzle, DrizzleD1Database } from 'drizzle-orm/d1';
-import { D1Database } from '@cloudflare/workers-types';
+import type { D1Database } from '@cloudflare/workers-types';
+import { drizzle } from 'drizzle-orm/d1';
 
-export function getDB(env: { DB: D1Database }): DrizzleD1Database {
+export function getDB(env: { DB: D1Database }) {
 	if (!env || !env.DB) {
 		throw new Error('D1 Database binding is missing in the environment variables');
 	}
