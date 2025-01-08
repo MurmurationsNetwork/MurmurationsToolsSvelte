ccfb50814192af6ae596af7991a5b92c8a7061de
vthwang <vincent@fishboneapps.com>
2025-01-08 00:11:43 -0800

    fix: mount the d1 directory to the local machine
    Signed-off-by: vthwang <vincent@fishboneapps.com>

--- src/routes/profiles/[profile_cuid]/+server.ts
+++ src/routes/profiles/[profile_cuid]/+server.ts
@@ -1,23 +1,33 @@
-import { connectToDatabase } from '$lib/db';
+import { getDB } from '$lib/db/db';
+import { profiles } from '$lib/db/migrations/schema';
+import type { D1Database } from '@cloudflare/workers-types';
 import { json, type RequestHandler } from '@sveltejs/kit';
+import { eq } from 'drizzle-orm';
 
-export const GET: RequestHandler = async ({ params }) => {
+export const GET: RequestHandler = async ({
+	params,
+	platform = { env: { DB: {} as D1Database } }
+}) => {
 	const { profile_cuid } = params;
 
 	if (!profile_cuid) {
 		return json({ success: false, error: 'Profile CUID is required' }, { status: 400 });
 	}
 
-	const db = await connectToDatabase();
+	const db = getDB(platform.env);
 
 	try {
-		const profile = await db.collection('profiles').findOne({ cuid: profile_cuid });
+		const profile = await db
+			.select()
+			.from(profiles)
+			.where(eq(profiles.cuid, profile_cuid))
+			.limit(1);
 
-		if (!profile) {
+		if (profile.length === 0) {
 			return json({ success: false, error: 'Profile not found' }, { status: 404 });
 		}
 
-		const parsedProfile = JSON.parse(profile.profile);
+		const parsedProfile = JSON.parse(profile[0].profile ?? '{}');
 
 		return json(parsedProfile);
 	} catch (error) {
